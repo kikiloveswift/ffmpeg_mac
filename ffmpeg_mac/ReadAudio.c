2c7cc026509925beff45fc0b5b6093d8b2d7d06a
konglee <konglee873@gmail.com>
2020-07-14 01:02:48 +0800

    auto push ffmpeg

--- ffmpeg_mac/ReadAudio.c
+++ ffmpeg_mac/ReadAudio.c
@@ -9,6 +9,29 @@
 #include "ReadAudio.h"
 
 void ffmpegTestReadAudio() {
+    
+    AVFormatContext *fmt_ctx = NULL;
+    
+    AVDictionary *options = NULL;
+    
+    /// [[ video device] : [audio device]]
+    char *devicename = ":0";
+    
+    // 1. register audio device
+    avdevice_register_all();
+    
+    // 2. get format
+    AVInputFormat *iFormate = av_find_input_format("avfoundation");
+    int ret = avformat_open_input(&fmt_ctx, devicename, iFormate, &options);
+    if (ret < 0) {
+        // error
+        char error[1024];
+        av_strerror(ret, error, sizeof(error));
+        printf(stderr, "Failed to open Audio device, [%d]%s\n", ret, error);
+        return;
+    }
+    
+    
     av_log_set_level(AV_LOG_DEBUG);
     av_log(NULL, AV_LOG_DEBUG, __func__);
     av_log(NULL, AV_LOG_DEBUG, "\n");
